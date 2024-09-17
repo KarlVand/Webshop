99b699c12c0b4130ae9795efe62698ec7f6d1c68
KarlVand <cg.vandooren.karl@gmail.com>
2024-09-17 22:57:13 +0200

    Header 2

--- webshop/src/components/Header.jsx
+++ webshop/src/components/Header.jsx
@@ -5,42 +5,61 @@ import BurgerMenu from "../assets/burger-menu.svg";
 function Header() {
   return (
     <header>
-      <div className="logo">
-        <img
-          src={Logo}
-          alt="logo"
-        />
-        <h1>FreshShop</h1>
+      <div className="banner">
+        <p></p>
+        <ul>
+          <li>
+            <a href="Account">My Account</a>
+          </li>
+          <li>
+            <a href="Location">Our Location</a>
+          </li>
+          <li>
+            <a href="Contact">Contact Us</a>
+          </li>
+        </ul>
       </div>
+      <div className="navbar">
+        <div className="logo">
+          <img
+            src={Logo}
+            alt="logo"
+          />
+          <h1>
+            <span className="fresh">Fresh</span>
+            <span className="shop">Shop</span>
+          </h1>
+        </div>
 
-      <ul className="links">
-        <li>
-          <a href="HOME">Home</a>
-        </li>
-        <li>
-          <a href="ABOUT US">About us</a>
-        </li>
-        <li>
-          <a href="SHOP">Shop</a>
-        </li>
-        <li>
-          <a href="GALLERY">Gallery</a>
-        </li>
-        <li>
-          <a href="CONTACT US">Contact us</a>
-        </li>
-      </ul>
+        <ul className="links">
+          <li>
+            <a href="Home">Home</a>
+          </li>
+          <li>
+            <a href="About">About us</a>
+          </li>
+          <li>
+            <a href="Shop">Shop</a>
+          </li>
+          <li>
+            <a href="Gallery">Gallery</a>
+          </li>
+          <li>
+            <a href="Contact">Contact us</a>
+          </li>
+        </ul>
 
-      <div className="buttons">
-        <a href="My Account">My Account</a>
-        <a href="My Cart">My Cart</a>
-      </div>
+        <div className="buttons">
+          <a href="My Account">My Account</a>
+          <a href="My Cart">My Cart</a>
+        </div>
 
-      <div className="burger-menu">
-        <img
-          src={BurgerMenu}
-          alt="Menu"
-        />
+        <div className="burger-menu">
+          <img
+            src={BurgerMenu}
+            alt="Menu"
+          />
+        </div>
       </div>
     </header>
   );
